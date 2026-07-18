0dcd518297bccac668abf11429ccd4bb82a464f7
Shikibashi <tshef41@gmail.com>
2026-07-18 15:11:34 -0400

    Apply semantic review to runtime question bank

---
+++ src/data/effectiveQuestions.ts
@@ -0,0 +1,25 @@
+import type { Question } from '../types'
+import {
+  allQuestions as rawAllQuestions,
+  coreQuestions as rawCoreQuestions,
+  getBankFingerprint as getRawBankFingerprint,
+  questionsForTier as rawQuestionsForTier,
+  SCORING_VERSION,
+} from './questions'
+import { applySemanticReview, SEMANTIC_AUDIT_VERSION } from './semanticAudit'
+
+export const QUESTION_BANK_VERSION = SEMANTIC_AUDIT_VERSION
+export { SCORING_VERSION }
+
+export function getBankFingerprint(): string {
+  return `${getRawBankFingerprint()}+${SEMANTIC_AUDIT_VERSION}`
+}
+
+export const coreQuestions: Question[] = rawCoreQuestions.map(applySemanticReview)
+export const questions: Question[] = coreQuestions
+export const allQuestions: Question[] = rawAllQuestions.map(applySemanticReview)
+export const questionById = new Map(allQuestions.map((question) => [question.id, question]))
+
+export function questionsForTier(tier: Question['tier']): Question[] {
+  return rawQuestionsForTier(tier).map(applySemanticReview)
+}
