961f1b1402a83dcc3b1c66f673fd3d7025ebe016
Shikibashi <tshef41@gmail.com>
2026-08-10 12:50:55 -0400

    Load desktop results layout overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './desktop-layout.css'
 import { DesktopShell } from './components/DesktopShell'
 import { ErrorBoundary } from './components/ErrorBoundary'
 
